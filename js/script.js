b29702d882f088c027bb1f27e6cd6e4e0261c03b
Daniel Oliveira <danielsapup3@gmail.com>
2022-11-21 14:53:24 -0300

    Mobile navbar toggle

--- js/script.js
+++ js/script.js
@@ -9,4 +9,18 @@ window.addEventListener("load", function() {
     loadElement.classList.add("loaded");
 });
 
-/* =======  ======= */
+
+/* ======= MOBILE NAVBAR TOGGLE ======= */
+const navbar = document.querySelector("[data-navbar]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+
+const toggleNavbar = function() {
+    navbar.classList.toggle("active");
+    navToggler.classList.toggle("active");
+};
+
+navToggler.addEventListener("click", toggleNavbar);
+
+
+
+
