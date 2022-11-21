6aca4ec97beacd66f4cce2c681e3fc11e468ddd7
Daniel Oliveira <danielsapup3@gmail.com>
2022-11-21 15:48:18 -0300

    Hero section

--- js/script.js
+++ js/script.js
@@ -22,5 +22,33 @@ const toggleNavbar = function() {
 navToggler.addEventListener("click", toggleNavbar);
 
 
+/* ======= HEADER ======= */
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-go-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
 
 
+/* SCROLL REVEAL */
+const revealElements = document.querySelectorAll("[data-reveal]");
+
+const scrollReveal = function () {
+  for (let i = 0, x = revealElements.length; i < x; i++) {
+    if (revealElements[i].getBoundingClientRect().top < window.innerHeight / 1.2) {
+      revealElements[i].classList.add("revealed");
+    } else {
+      revealElements[i].classList.remove("revealed");
+    }
+  }
+}
+
+window.addEventListener("scroll", scrollReveal);
+window.addEventListener("load", scrollReveal);
